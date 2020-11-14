411e1bf2439772772efb70fc0e97806071442d85
Worth Winchester <worthhw@gmail.com>
2020-11-13 23:18:36 -0500

    Modify password_box.py: add encrypt() and decrypt() functions; Add test input file

--- password_box.py
+++ password_box.py
@@ -3,6 +3,7 @@ import os
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
 import getpass
 
 #to generate salt run os.urandom(16)
@@ -25,19 +26,40 @@ def get_key(password, salt):
     return key
 
 def get_salt():
-    if os.path.isfile("salt.txt") == False:
+    if os.path.isfile("salt_file") == False:
         salt = os.urandom(16)
-        with open("salt.txt", "w") as f:
-            f.write(str(salt))
+        with open("salt_file", "wb") as f:
+            f.write(salt)
     else:
-        with open("salt.txt", "r")as f:
-            salt = f.read().encode()
+        with open("salt_file", "rb")as f:
+            salt = f.read()
     return salt
 
-password = get_password()
-salt = get_salt()
-key = get_key(password, salt)
+def encrypt(key, input_file):
+    fernet = Fernet(key)
+    with open(input_file, "rb") as f:
+        original_data = f.read()
+    encrypted = fernet.encrypt(original_data)
+    with open("encrypted_file", "wb") as f:
+        f.write(encrypted)
 
+def decrypt(key, encrypted_file):
+    fernet = Fernet(key)
+    with open(encrypted_file, "rb") as f:
+        encrypted_data = f.read()
+    decrypted = fernet.decrypt(encrypted_data)
+    with open("decrypted_file", 'wb') as f:
+        f.write(decrypted)
+
+password = get_password()
 print(password)
+salt = get_salt()
 print(salt)
+key = get_key(password, salt)
 print(key)
+input_file = "input.txt"
+encrypt(key, input_file)
+print("File Encrypted")
+encrypted_file = "encrypted_file"
+decrypt(key, encrypted_file)
+print("File Decrypted")
